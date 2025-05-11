7414026e261abb35ed4937248dcaf33f3eb4b075
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2025-05-11 14:13:11 +0000

    Added configuration for generating types information into dist folder.

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'));
 
@@ -66,5 +67,34 @@ export default [
 				plugins: [terser()]
 			}
 		]
-	}
+	},
+	{
+		input: './src/index.esm.ts',
+		external: [
+			'chart.js',
+			'chart.js/helpers'
+		],
+		plugins: [
+			resolve(), typescript()
+		],
+		output: {
+			sourcemap: true,
+			banner: banner,
+			globals: {
+				'chart.js': 'Chart',
+				'chart.js/helpers': 'Chart.helpers'
+			},
+			file: pkg.types,
+			format: 'es',
+		},
+		plugins: [
+			dts({
+				respectExternal: true,
+				compilerOptions: {
+					skipLibCheck: true,
+					skipDefaultLibCheck: true,
+				},
+			}),
+		],
+	},
 ]
